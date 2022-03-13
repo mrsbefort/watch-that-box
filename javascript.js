1e27d0b01686a486fe96653db0f8af0b672a2d1e
Gabrielle Befort <mrsbefort@gmail.com>
2022-03-13 12:26:00 -0400

    adding js and html

---
+++ javascript.js
@@ -0,0 +1,16 @@
+function fade() {
+    document.getElementById("box").style.opacity="0.5";
+}
+function changeColor() {
+     document.getElementById ("box") .style.backgroundColor="blue";
+}
+function growBtn() {
+    document.getElementById ("box") .style.height="300px"; 
+    document.getElementById ("box") .style.width="300px";
+}
+function reset() {
+    document.getElementById ("box")     .style.height="150px"; 
+    document.getElementById ("box") .style.width="150px";
+    document.getElementById ("box") .style.backgroundColor="orange";
+    document.getElementById("box").style.opacity="1";
+}
